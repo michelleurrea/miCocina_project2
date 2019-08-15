d6eb5cdc2adacc514e8b148618934423855c8395
h64 <hhongnr@gmail.com>
2019-08-15 16:57:53 -0700

    Finished auth routes, and styled the login form

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,10 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 })
 
+router.post('/login', (req, res) => {
+    res.send('Stub - ToDo: Log in, then redirect');
+})
+
 router.get('/logout', (req, res) => {
     res.render('auth/logout');
 })
